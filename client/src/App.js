acc590bdf52790285fb1d67552831ac6aebbde79
John <nadenjohn@gmail.com>
2022-05-23 09:51:35 -0700

    tailwinds set-up, some light css attempts

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Header from './components/Header'
+import MyReservations from  "./components/MyReservations"
+import Home from "./components/Home"
+import OurVenues from "./components/OurVenues"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-container">
+      <div className="bg-indigo-600 w-full h-screen">
+          <Header />
+
+        <div id="main-area">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/myreservations" element={<MyReservations />} />
+            <Route path="/ourvenues" element={<OurVenues />}/>
+          </Routes>
+        </div>
     </div>
-  );
+  </div>
+
+)
 }
 
-export default App;
+export default App
---
+++ client/src/components/MyReservations.js
@@ -0,0 +1,12 @@
+function MyReservations() {
+    return (
+      <div className="content">
+        <h2>MyReservations</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
+        
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
+      </div>
+    )
+  }
+
+  export default MyReservations;
