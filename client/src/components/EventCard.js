74d0dd8a6d9cb8349aacc1d07926b18388f1d4ae
John <nadenjohn@gmail.com>
2022-05-24 10:36:09 -0700

    date filters implemented

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -2,11 +2,18 @@ import React from "react";
 
 
 function EventCard({event}) {
+
+    const date=new Date(event.event_date).toLocaleString()
+    console.log(date)
+
+    const displayDate = date.slice(0, -6)
+
     return (
 <div className="max-w-sm rounded overflow-hidden shadow-lg">
   <img className="w-full" src="https://f4.bcbits.com/img/0027801541_0" alt="Sunset in the mountains"/>
   <div className="px-6 py-4">
     <div className="font-bold text-xl mb-2">{event.event_name}</div>
+    <div className="font-bold text-xl mb-2">{displayDate} PM</div>
     <p className="text-gray-700 text-base">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
     </p>
