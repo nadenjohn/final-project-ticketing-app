c81129a344f3f60ebffad59823b2eb694c073bad
John <nadenjohn@gmail.com>
2022-05-31 13:05:32 -0700

    calendar works functionally

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -3,6 +3,7 @@ import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import { useState } from 'react';
+import CalendarItem from './CalenderItem'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -118,88 +119,12 @@ const handleForm = (e) => {
                       </div>
                     </div>
                   </div>
+                  <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
+                        Event Dates
+                  </label>
+                  <CalendarItem />
                   <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
-                      <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
-                        Event Date
-                      </label>
-                      <Menu as="div" className=" p-0 relative inline-block text-left">
-                      <div>
-                        <Menu.Button id ="event-description" name="event-description" className=" inline-flex w-full rounded-none border rounded-r-md border-gray-300 shadow-sm px-2 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                          Event Date
-                          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-                        </Menu.Button>
-                      </div>
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        <Menu.Items className="origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <div className="py-1">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href="#"
-                                  className={classNames(
-                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                    'block px-4 py-2 text-sm'
-                                  )}
-                                >
-                                  Music
-                                </a>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href="#"
-                                  className={classNames(
-                                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                  'block px-4 py-2 text-sm'
-                                )}
-                              >
-                                All Ages
-                              </a>
-                            )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href="#"
-                                  className={classNames(
-                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                    'block px-4 py-2 text-sm'
-                                  )}
-                                >
-                                  Comedy
-                                </a>
-                              )}
-                            </Menu.Item>
-                            <form method="POST" action="#">
-                              <Menu.Item>
-                                {({ active }) => (
-                                  <button
-                                    type="submit"
-                                    className={classNames(
-                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                      'block w-full text-left px-4 py-2 text-sm'
-                                    )}
-                                  >
-                                    Burlesque
-                                </button>
-                              )}
-                            </Menu.Item>
-                          </form>
-                        </div>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                  </div>
+                  
                   
                     <div className="col-span-3 sm:col-span-2">
                       <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
