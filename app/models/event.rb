5a29f3a2d299b41ac0877e3dda22d752dc07dd3d
John <nadenjohn@gmail.com>
2022-06-02 00:42:03 -0700

    working ok....

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :venue
     has_many :tickets
+    has_many :cart_items
     has_many :users, through: :tickets
 end
--- client/src/components/MyReservations.js
+++ client/src/components/MyReservations.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import EventCard from './EventCard'
+
+import MyEventsItem from './MyEventsItem'
 
 function MyReservations({events, user, handlePost}) {
     return (
@@ -8,33 +9,7 @@ function MyReservations({events, user, handlePost}) {
         <h2 className="text-lg font-medium text-gray-900">My Reservations</h2>
         <div className="mt-6 pb-10 border-t border-b border-gray-200 divide-y divide-gray-200 space-y-10">
           {events.map((event) => (
-            <div key={event.event_id} className="pt-10 lg:grid lg:grid-cols-12 lg:gap-x-8">
-              <div className="lg:col-start-5 lg:col-span-8 xl:col-start-4 xl:col-span-9 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:items-start">
-                <div className="flex items-center xl:col-span-1">
-
-                  <p className="ml-3 text-sm text-gray-700">
-                    {event.price}
-                    <span className="sr-only"> out of 5 stars</span>
-                  </p>
-                </div>
-
-                <div className="mt-4 lg:mt-6 xl:mt-0 xl:col-span-2">
-                  <h3 className="text-sm font-medium text-gray-900">{event.event_name}</h3>
-
-                  
-                </div>
-              </div>
-
-              <div className="mt-6 flex items-center text-sm lg:mt-0 lg:col-start-1 lg:col-span-4 lg:row-start-1 lg:flex-col lg:items-start xl:col-span-3">
-                <p className="font-medium text-gray-900">{event.price}</p>
-                <time
-                  dateTime={event.event_name}
-                  className="ml-4 border-l border-gray-200 pl-4 text-gray-500 lg:ml-0 lg:mt-2 lg:border-0 lg:pl-0"
-                >
-                  {event.event_date}
-                </time>
-              </div>
-            </div>
+            <MyEventsItem key={event.id} event={event}/>
           ))}
         </div>
       </div>
