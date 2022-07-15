2f2f9e4d1a9d93e4260bcc3f113ce77c2eede344
John <nadenjohn@gmail.com>
2022-07-15 15:04:37 -0700

    html indentation looks better

--- client/src/components/MyReservations.js
+++ client/src/components/MyReservations.js
@@ -1,22 +1,18 @@
-import React from 'react'
-
-import MyEventsItem from './MyEventsItem'
+import React from 'react';
+import MyEventsItem from './MyEventsItem';
 
 function MyReservations({myReservations}) {
-
-  
-    return (
-      <div className="bg-slate-400'">
+  return (
+    <div className="bg-slate-400'">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-lg font-medium text-gray-900">My Reservations</h2>
         <div className="mt-6 pb-10 border-t border-b border-gray-200 divide-y divide-gray-200 space-y-10">
           {myReservations.map((reservation) => (
-            <MyEventsItem key={reservation.id} reservation={reservation}/>
+          <MyEventsItem key={reservation.id} reservation={reservation}/>
           ))}
         </div>
       </div>
     </div>
-    )
-  }
+  )};
 
   export default MyReservations;
--- client/src/components/VenueCard.js
+++ client/src/components/VenueCard.js
@@ -1,22 +1,16 @@
 import React from "react";
 
-
 function VenueCard({venue}) {
-    console.log(venue)
-    return (
-
-            <div className=" bg-neutral-300 border-r border-b border-l border-t border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                <img className="h-100 w-100 flex rounded-t lg:rounded-t-none lg:rounded-l  object-center" src={venue.image}/>
-                <div className="mb-8">
-                    <p className="text-sm text-gray-600 flex items-center"></p>
-                    <div className="text-gray-900 font-bold text-xl mb-2">{venue.name}</div>
-                    <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-                </div>
-        </div>
-
-   
+  console.log(venue);
+  return (
+    <div className="bg-neutral-300 border-r border-b border-l border-t border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+      <img className="h-100 w-100 flex rounded-t lg:rounded-t-none lg:rounded-l  object-center" src={venue.image}/>
+      <div className="mb-8">
+        <p className="text-sm text-gray-600 flex items-center"></p>
+        <div className="text-gray-900 font-bold text-xl mb-2">{venue.name}</div>
+        <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+      </div>
+    </div>
+  )};
   
-    )
-  }
-  
-  export default VenueCard;
+export default VenueCard;
